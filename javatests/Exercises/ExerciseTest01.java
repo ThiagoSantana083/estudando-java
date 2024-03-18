81a5a97015327c79c3ab47c3ffa4feebc9df7723
Thiago Santana <thiagosantanara654@gmail.com>
2024-03-18 17:23:00 -0300

    Criação da pasta Exercises que é os exercícios dos estudo que faço de programação

---
+++ javatests/Exercises/ExerciseTest01.java
@@ -0,0 +1,14 @@
+package javatests.Exercises;
+
+public class ExerciseTest01 {
+    public static void main(String[] args) {
+        // Crie um método que mostre "Olá, meu nome é Thiago Santana e estudo programação! " Utilizando variáveis do
+        // tipo String com - Nome, Sobrenome e disciplina
+
+        String nome = "Thiago";
+        String sobrenome = "Santana";
+        String disciplina = "programação";
+
+        System.out.println("Olá, meu nome é " + nome + " " + sobrenome + " e estudo " + disciplina + "!");
+    }
+}
