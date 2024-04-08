50254dcaa90320536bbd43a10e06841a3058d9cd
Thiago Santana <thiagosantanara654@gmail.com>
2024-04-08 11:27:51 -0300

    Atualização dos exercícios feitos pelo chatgpt, exercicio 01, 02 feitos

--- javatests/Exercises/ExercisesGPT/Pessoa.java
+++ javatests/Exercises/ExercisesGPT/Pessoa.java
@@ -1,8 +1,21 @@
 package javatests.Exercises.ExercisesGPT;
 
 public class Pessoa {
+
+    // Exercício 01
     private String nome;
     private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // Exercício 02
+    public void detalhes () {
+        System.out.println("Nome da pessoa é: " + this.getNome());
+        System.out.println("Idade da pessoa é: " + this.getIdade());
+    }
 
     public String getNome() {
         return nome;
