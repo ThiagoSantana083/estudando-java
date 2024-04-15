d6c948b355c524e83ad6db52750045c556e3ce33
Thiago Santana <thiagosantanara654@gmail.com>
2024-04-15 17:14:52 -0300

    Criação da classe de exercício 26-31. Exercício 26 concluído

--- javatests/Exercises/ExercisesGPT/Animal.java
+++ javatests/Exercises/ExercisesGPT/Animal.java
@@ -4,4 +4,28 @@ public class Animal {
     // Exercício 26
     private String nome;
     private int idade;
+
+    public void emitirSom() {
+        System.out.println("Emitiu som!");
+    }
+
+    public void mover() {
+        System.out.println("Se moveu!");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
